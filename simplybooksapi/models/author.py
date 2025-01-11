55685de675456784ccd182a705622a5b31cf5ac7
Abby Paradise <162839518+asparadise33@users.noreply.github.com>
2025-01-11 13:09:11 -0600

    Initial commit

---
+++ simplybooksapi/models/author.py
@@ -0,0 +1,9 @@
+#rom django.db import models
+
+
+#class Author(models.Model):
+ #email = models.CharField(max_length=80)
+ #first_name = models.CharField(max_length=80)
+ #last_name = models.CharField(max_length=80)
+ #image = models.CharField(max_length=100)
+ #favorite = models.BooleanField(default=True)
---
+++ simplybooksapi/models/book.py
@@ -0,0 +1,10 @@
+#from django.db import models
+#from .author import Author
+
+#class Book(models.Model):
+ #author_id =  models.ForeignKey(Author, on_delete=models.CASCADE)
+ #title = models.CharField(max_length=80)
+ #image = models.CharField(max_length=80)
+ #price = models.IntegerField()
+ #sale =  models.BooleanField(default=True)
+ #description = models.CharField(max_length=200)
